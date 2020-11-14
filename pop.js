a1927e4fc9abde953e3b2d2090de853d683f1a25
abhijitXD <abhihota025@gmail.com>
2020-11-14 11:34:51 +0530

    change: uppercase logic

--- pop.js
+++ pop.js
@@ -1,43 +1,43 @@
+// You can make this configurable by the user
+const LIMIT = 0.5;
 
 function isLetter(str) {
-  return str.length === 1 && str.match(/[a-z]/i);
+    return str.length === 1 && str.match(/[a-z]/i);
 }
 
 function updateClipboard(newClip) {
-  navigator.clipboard.writeText(newClip).then(function() {
-    /* clipboard successfully set */
-  }, function() {
-    /* clipboard write failed */
-  });
+    navigator.clipboard.writeText(newClip).then(
+        function () {
+            /* clipboard successfully set */
+        },
+        function () {
+            /* clipboard write failed */
+        }
+    );
 }
-function getClipboard(){
-		
+function getClipboard() {
+    var t = document.createElement("input");
+    document.body.appendChild(t);
+    t.focus();
+    document.execCommand("paste");
+    var clipboardText = t.value; //this is your clipboard data
 
+    arr_text = clipboardText.split("");
+    for (let i = 0; i < arr_text.length; i++) {
+		const random = Math.random();
 
-	var t = document.createElement("input");
-	document.body.appendChild(t);
-	t.focus();
-	document.execCommand("paste");
-	var clipboardText = t.value; //this is your clipboard data
-	
-
-
-	arr_text = clipboardText.split("");
-		for (let i = 0 ; i< arr_text.length ; i++)
-		{
-			if(i%2 == 0){
-				if(isLetter(arr_text[i])){
-					arr_text[i] = arr_text[i].toUpperCase();
-				}
-			}
-		}
-		final  = arr_text.join("");
-	updateClipboard(final); 
-	document.body.removeChild(t);
-
-
+		// 50% chance of uppercasing
+        if (random < LIMIT) {
+            if (isLetter(arr_text[i])) { // This check is not needed
+                arr_text[i] = arr_text[i].toUpperCase();
+            }
+        }
+    }
+    final = arr_text.join("");
+    updateClipboard(final);
+    document.body.removeChild(t);
 }
-document.addEventListener('DOMContentLoaded', function() {
-var gopy = document.getElementById('gopy');
-	gopy.addEventListener('click',getClipboard)
-})
+document.addEventListener("DOMContentLoaded", function () {
+    var gopy = document.getElementById("gopy");
+    gopy.addEventListener("click", getClipboard);
+});
